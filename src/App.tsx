1ceee15be68aa7c4c9c59cc3efbc762218487a59
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-04-17 00:24:12 +0530

    props

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Hello } from './components/Hello';
 import { Person} from './components/Person';
 import { PersonList } from './components/PersonList';
 import './App.css';
+import { Status } from './components/Status';
 
 function App() {
   const personName = {
@@ -28,7 +29,9 @@ function App() {
       <Hello name="Birat" messageCount={10} isLoggedIn={false} />
       <Person name = {personName} />
       <PersonList  names = {nameList}/>
-      
+      <Status status='loading' />
+      <Status status='success' />
+      <Status status='error' />
     </div>
   );
 }
